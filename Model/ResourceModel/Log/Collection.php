0c426a6b393924473f30def481de95807acea567
pankitp-tbs <pankitp.tbsdev@gmail.com>
2026-02-02 14:58:11 +0530

    m237 change some code

--- Model/ResourceModel/Log/Collection.php
+++ Model/ResourceModel/Log/Collection.php
@@ -6,6 +6,11 @@ use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 class Collection extends AbstractCollection
 {
     /**
+     * @var string
+     */
+    protected $_idFieldName = 'log_id';
+
+    /**
      * Define resource model
      *
      * @return void
